f586ab9e07cdd0e69cf3ca781952eee58ae21a16
Jobin Lawrance <jobin@supaki.io>
2024-09-29 21:36:52 +0530

    Add a coming soon page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Disable linting during builds
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
